a2e793de4121678fd67779d330061d3363768ffd
William <126926699+GitXpresso@users.noreply.github.com>
2025-05-25 19:22:07 +0000

    Update guac-installer.sh

--- guac-installer.sh
+++ guac-installer.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-
+GUACAMOLE_VERSION="1.5.5"
 if [ -d /etc/dnf ]; then
 packages=("wget" "cairo-devel" "libjpeg-devel" "libpng-devel" "uuid-devel" "freerdp-devel" "pango-devel" "libssh2-devel" "libtelnet-devel" "libvncserver-devel" "pulseaudio-libs-devel" "openssl-devel" "libvorbis-devel" "libwebsockets-devel" "tomcat-native" "tomcat")
 for dnfpackages in "$(packages[@])'; do
      sudo dnf list installed | grep "$packages[@]" > /dev/null 2>&1
      if [ $? -ne 0 ]; then
-         echo "$packages[@] is not installed"
+         export installed="No"
+         echo "$packages[@] installed [$Installed]"
          sudo dnf install -y "$packages[@]"
+         sleep 0.5
+         clear
      else
-         echo "$packages[@] is installed"
+         export installed2="Ok"
+         echo "$packages[@] $installed2"
      fi
 done
 fi
+wget -q --show-progress https://downloads.apache.org/guacamole/$GUACAMOLE_VERSION/source/guacamole-server-$GUACAMOLE_VERSION.tar.gz
