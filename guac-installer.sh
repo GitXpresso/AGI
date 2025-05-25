0b3beb70e0d290f217c450cdecd368f5752b97ae
William <126926699+GitXpresso@users.noreply.github.com>
2025-05-25 19:35:50 +0000

    Update guac-installer.sh

--- guac-installer.sh
+++ guac-installer.sh
@@ -40,4 +40,72 @@ fi
 echo "moving war file to tomcat webapps directory"
 sudo mv guacamole-$GUACAMOLE_VERSION.war /var/lib/tomcat/webapps/guacamole.war
 sudo mkdir -p /etc/guacamole/{extensions,lib}
+echo "GUACAMOLE_HOME=/etc/guacamole" >> tomcat && sudo mv tomcat /etc/default
+cat << EOF >./guacamole.properties
+# Guacamole properties
+
+# Hostname and port of guacamole proxy
+guacd-hostname: localhost
+guacd-port: 4822
+
+# Auth provider class (authenticates user credentials)
+# Example LDAP authentication
+#auth-provider: net.sourceforge.guacamole.net.basic.BasicFileAuthenticationProvider
+# Example MySQL authentication
+#auth-provider: net.sourceforge.guacamole.net.mysql.MySQLAuthenticationProvider
+# Example JDBC authentication
+#auth-provider: net.sourceforge.guacamole.net.jdbc.JDBCAuthenticationProvider
+
+# LDAP properties
+#ldap-hostname: ldap.example.com
+#ldap-port: 389
+#ldap-user-base-dn: dc=example,dc=com
+#ldap-username-attribute: uid
+#ldap-encryption-method: none
+#ldap-search-bind-dn: cn=admin,dc=example,dc=com
+#ldap-search-bind-password: password
+
+# MySQL properties
+#mysql-hostname: localhost
+#mysql-port: 3306
+#mysql-database: guacamole_db
+#mysql-username: guacamole_user
+#mysql-password: password
+
+# JDBC properties
+#jdbc-driver: com.mysql.jdbc.Driver
+#jdbc-url: jdbc:mysql://localhost:3306/guacamole_db
+#jdbc-username: guacamole_user
+#jdbc-password: password
+
+# Guacamole authentication
+# Example basic file authentication
+#basic-user-mapping: /etc/guacamole/user-mapping.xml
+
+# Guacamole properties
+# The value of "guacd-hostname" is ignored if using "guacd-socket".
+#guacd-hostname: localhost
+#guacd-port: 4822
+#guacd-socket: /var/run/guacd/guacd.sock
+
+# Enable SSL
+#guacamole-ssl: true
+#guacamole-ssl-certificate: /etc/pki/tls/certs/localhost.crt
+#guacamole-ssl-key: /etc/pki/tls/private/localhost.key
+#guacamole-ssl-key-password: password
+
+# WebSocket configuration
+#web-socket-support: true
+#web-socket-maximum-connections: 100
+
+# Token parameter name
+#token-parameter-name: token
+
+# User permissions
+#admin-group: admin
+#user-group: users
+
+# Disable user input history
+#history-size: 0
+EOF
 
