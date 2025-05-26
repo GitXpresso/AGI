5c6218295366f19366b95ddf647bf677363c178b
William <126926699+GitXpresso@users.noreply.github.com>
2025-05-26 20:09:07 +0000

    Update guac-installer.sh

--- guac-installer.sh
+++ guac-installer.sh
@@ -32,8 +32,9 @@ if [ ! $(pwd) == "$HOME" ]; then
 fi
 
 echo "Downloading tarball"
+if [ ! -d $HOME/guacamole-server-$GUACAMOLE_VERSION ]; then
 wget -q --show-progress -o guacamole-server-$GUACAMOLE_VERSION.tar.gz https://apache.org/dyn/closer.lua/guacamole/1.5.5/source/guacamole-server-$GUACAMOLE_VERSION.tar.gz?action=download
-
+fi
 tar -xf guacamole-server-$GUACAMOLE_VERSION.tar.gz 
 if [ ! $(pwd) == $HOME/guacamole-server-$GUACAMOLE_VERSION ]; then
 cd guacamole-server-$GUACAMOLE_VERSION
@@ -48,7 +49,9 @@ clear
 
 echo "download war file..."
 #wget -q --show-progress https://downloads.apache.org/guacamole/$GUACAMOLE_VERSION/binary/guacamole-$GUACAMOLE_VERSION.war
+if [ ! -f $HOME/guacamole-$GUACAMOLE_VERSION.war ]; then
 wget -q --show-progress -o guacamole-$GUACAMOLE_VERSION.war https://apache.org/dyn/closer.lua/guacamole/$GUACAMOLE_VERSION/binary/guacamole-$GUACAMOLE_VERSION.war?action=download
+fi
 if [ ! rpm -qa | grep -q -o "tomcat" ]; then
    sudo dnf install tomcat
 fi
